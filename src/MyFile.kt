e9b5fc6572c434d23ac3b006b633a845241c2284
Samriddha <samriddha97@gmail.com>
2022-12-30 21:12:23 +0530

    marge two sorted array problem solved.

--- src/MyFile.kt
+++ src/MyFile.kt
@@ -1,26 +1,35 @@
 
 fun main(){
 
-    margeSortedArray(intArrayOf(1,3,6,7,9), intArrayOf(1,2,2,4,8)).forEach {
+    margeSortedArray(intArrayOf(1,3,6,7,99), intArrayOf(1,2,22,44)).forEach {
         println(it)
     }
 }
 
 fun margeSortedArray(array1: IntArray,array2: IntArray):IntArray{
-    val sizeOfArray = if (array1.size>array2.size) array1.size else array2.size
     var result = IntArray(array1.size+array2.size)
-    var currentPos=0
-    for (i in 0 until sizeOfArray){
-        if (array1[i]<=array2[i]){
-            result[currentPos]=array1[i]
-            currentPos++
-            result[currentPos]=array2[i]
-            currentPos++
+    var currentPos1=0
+    var currentPos2=0
+    var newPos = 0
+    for (i in result.indices){
+        if (currentPos1>array1.size-1) {
+            for (j in currentPos2 until array2.size){
+                result[newPos]=array2[j]
+                newPos++
+            }
+        }else if (currentPos2>array2.size-1) {
+            for (j in currentPos1 until array1.size){
+                result[newPos]=array1[j]
+                newPos++
+            }
+        }else if (array1[currentPos1]<=array2[currentPos2]){
+            result[newPos]=array1[currentPos1]
+            currentPos1++
+            newPos++
         }else{
-            result[currentPos]=array2[i]
-            currentPos++
-            result[currentPos]=array1[i]
-            currentPos++
+            result[newPos]=array2[currentPos2]
+            currentPos2++
+            newPos++
         }
     }
 
